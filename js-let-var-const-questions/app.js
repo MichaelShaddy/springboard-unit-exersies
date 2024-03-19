7b0a15aec92f748261b8853992b8cfe37f8fdac5
Michael Shaddy <114063621+MichaelShaddy@users.noreply.github.com>
2024-03-18 20:46:57 -0700

    added let/var/const exersise

---
+++ js-let-var-const-questions/app.js
@@ -0,0 +1,15 @@
+// - What is the difference between var and let?
+
+    // the main difference is that let cannot be redeclared while var can. as well as let being only in block scope while car is function scope 
+
+// - What is the difference between var and const?
+
+//const cannot be redeclared, and just like let, is block scope
+
+// - What is the difference between let and const?
+
+//both are block scope, cannot be redeclared but const cannot be reevaluated
+
+// - What is hoisting?
+
+//hoisting is the ability to call varibles, log them, etc before they are called. 'hoisted' to the top of functions or global scope. let and const can be hoisted, while var cannot.
